309a095caa2d73db6697f160a64961f1457d10e1
Muhammed Fouzan <fouzanpharmd@gmail.com>
2020-09-21 01:31:37 +0530

    added react bootstrap

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Jumbotron } from "react-bootstrap";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Admin Dashboard</h1>
+      <Jumbotron className="text-center">
+        <h1>Admin Dashboard</h1>
+      </Jumbotron>
     </div>
   );
 }
