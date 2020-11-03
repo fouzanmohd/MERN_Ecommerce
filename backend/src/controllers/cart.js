20afb98e247c024d56e60c8f1d9139629e0c4bef
Muhammed Fouzan <fouzanpharmd@gmail.com>
2020-11-03 23:19:31 +0530

    cart.js controller code fix and minor refactoring

--- backend/src/controllers/cart.js
+++ backend/src/controllers/cart.js
@@ -4,27 +4,25 @@ exports.addItemToCart = (req, res) => {
   Cart.findOne({ user: req.user._id }).exec((error, cart) => {
     if (error) return res.status(400).json({ error });
     if (cart) {
-       const isItemAdded =  cart.cartItems.find(c=>c.product == req.body.cartItems.product)
+       const product = req.body.cartItems.product
+       const isItemAdded =  cart.cartItems.find(c=>c.product == product)
+       let condition, action;
+      
        if (isItemAdded) {
-
-        Cart.findOneAndUpdate({user:req.user._id, "cartItems.product": req.body.cartItems.product}, {'$set':{
-            "cartItems": {...req.body.cartItems, quantity: isItemAdded.quantity + req.body.cartItems.quantity}
-            
-        }}).exec((error, _cart)=>{
-            if (error) return res.status(400).json({error})
-            if(_cart) return res.status(201).json({_cart})
-        })
-
-       }else {
-
-           Cart.findOneAndUpdate({user:req.user._id}, {'$push':{
-               "cartItems": req.body.cartItems
-               
-           }}).exec((error, _cart)=>{
-               if (error) return res.status(400).json({error})
-               if(_cart) return res.status(201).json({_cart})
-           })
+        condition = {user:req.user._id, "cartItems.product": req.body.cartItems.product};
+        action = {'$set':{"cartItems.$": {...req.body.cartItems, quantity: isItemAdded.quantity + req.body.cartItems.quantity}}}
        }
+
+       else {
+         condition = {user:req.user._id};
+         action = {'$push':{"cartItems": req.body.cartItems}}
+       }
+
+       Cart.findOneAndUpdate(condition, action).exec((error, _cart)=>{
+        if (error) return res.status(400).json({error})
+        if(_cart) return res.status(201).json({_cart})
+    })
+
     } else {
       const cart = new Cart({
         user: req.user._id,
